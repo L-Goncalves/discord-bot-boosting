05cdf1b99f15b9200c460616826654e1355fe91b
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-02-26 15:22:33 -0300

    feat: adding rest of functions to have it working

--- Commands/Slash/Misc/start.js
+++ Commands/Slash/Misc/start.js
@@ -20,23 +20,6 @@ export default {
     try {
       const { guild } = interaction;
 
-      // Create or find the "boosts" channel
-      let boostsChannel = guild.channels.cache.find(
-        (c) => c.name === "boosts" && c.type === ChannelType.GuildText
-      );
-      if (!boostsChannel) {
-        boostsChannel = await guild.channels.create({
-          name: "boosts",
-          type: ChannelType.GuildText,
-          permissionOverwrites: [
-            {
-              id: guild.id, // @everyone
-              deny: [PermissionsBitField.Flags.ViewChannel],
-            },
-          ],
-        });
-      }
-
       // Create or find the "logs" channel
       let logsChannel = guild.channels.cache.find(
         (c) => c.name === "logs" && c.type === ChannelType.GuildText
@@ -66,6 +49,64 @@ export default {
             {
               id: guild.id, // @everyone
               deny: [PermissionsBitField.Flags.ViewChannel],
+            },
+          ],
+        });
+      }
+
+      let supportRole = guild.roles.cache.find(
+        (role) => role.name === "Support"
+      );
+      if (!supportRole) {
+        supportRole = await guild.roles.create({
+          name: "Support",
+          color: 0x800080, // Purple color
+          permissions: [], // Add permissions if needed
+        });
+      }
+
+      let dpsRole = guild.roles.cache.find((role) => role.name === "DPS");
+      if (!dpsRole) {
+        dpsRole = await guild.roles.create({
+          name: "DPS",
+          color: 0x800080, // Purple color
+          permissions: [], // Add permissions if needed
+        });
+      }
+
+      let tankRole = guild.roles.cache.find((role) => role.name === "Tank");
+      if (!tankRole) {
+        tankRole = await guild.roles.create({
+          name: "Tank",
+          color: 0x800080, // Purple color
+          permissions: [], // Add permissions if needed
+        });
+      }
+
+      // Create or find the "boosts" channel
+      let boostsChannel = guild.channels.cache.find(
+        (c) => c.name === "boosts" && c.type === ChannelType.GuildText
+      );
+      if (!boostsChannel) {
+        boostsChannel = await guild.channels.create({
+          name: "boosts",
+          type: ChannelType.GuildText,
+          permissionOverwrites: [
+            {
+              id: guild.id, // @everyone
+              deny: [PermissionsBitField.Flags.ViewChannel],
+            },
+            {
+              id: tankRole.id, // @everyone
+              allow: [PermissionsBitField.Flags.ViewChannel],
+            },
+            {
+              id: dpsRole.id, // @everyone
+              allow: [PermissionsBitField.Flags.ViewChannel],
+            },
+            {
+              id: supportRole.id, // @everyone
+              allow: [PermissionsBitField.Flags.ViewChannel],
             },
           ],
         });
